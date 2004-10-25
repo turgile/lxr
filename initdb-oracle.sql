07e0da1e4dfbf73049ab4e0e44fedc9c06ea2e99
Dave Brondsema <dave@brondsema.net>
2004-10-25 20:18:50 +0000

    update with new tables/columns

--- initdb-oracle.sql
+++ initdb-oracle.sql
@@ -1,16 +1,41 @@
+-- This assumes you have a user 'lxr' set up already.
+
 drop sequence lxr_filenum;
 drop sequence lxr_symnum;
-drop table lxr_indexes;
-drop table lxr_usage;
-drop table lxr_symbols;
-drop table lxr_releases;
-drop table lxr_status;
-drop table lxr_files;
+drop sequence lxr_declnum;
+drop table    lxr_indexes;
+drop table    lxr_declarations;
+drop table    lxr_usage;
+drop table    lxr_symbols;
+drop table    lxr_releases;
+drop table    lxr_status;
+drop table    lxr_files;
 
 commit;
 
-create sequence lxr_filenum;
-create sequence lxr_symnum;
+create sequence lxr_filenum
+INCREMENT BY 1   
+START WITH 1    
+NOMAXVALUE       
+NOMINVALUE       
+CACHE 5          
+NOORDER;         
+
+create sequence lxr_symnum
+INCREMENT BY 1   
+START WITH 1    
+NOMAXVALUE       
+NOMINVALUE       
+CACHE 5          
+NOORDER;         
+
+create sequence lxr_declnum
+INCREMENT BY 1   
+START WITH 1    
+NOMAXVALUE       
+NOMINVALUE       
+CACHE 5          
+NOORDER;         
 
 commit;
 
@@ -20,7 +45,9 @@ create table lxr_files (
 	fileid		number,
 	constraint lxr_pk_files primary key (fileid)
 );
+
 alter table lxr_files add unique (filename, revision);
+
 create index lxr_i_files on lxr_files(filename);
 
 commit;
@@ -28,9 +55,19 @@ commit;
 create table lxr_symbols (				
 	symname		varchar2(250),
 	symid		number,
-	constraint lxr_pk_symbols primary key (symid)
+	constraint pk_lxr_symbols primary key (symid)
 );
+
 alter table lxr_symbols add unique(symname);
+
+commit;
+
+create table lxr_declarations (
+	declid		number NULL,
+	langid		number NULL,
+	declaration	varchar2(255),
+	constraint pk_lxr_declarations primary key (declid)
+);
 
 commit;
 
@@ -38,12 +75,17 @@ create table lxr_indexes (
 	symid		number,
 	fileid		number,
 	line		number,
-	type		varchar2(250),
+	langid		number,
+	type		number,
 	relsym		number,
-	constraint lxr_fk_indexes_fileid foreign key (fileid) references lxr_files(fileid),
-	constraint lxr_fk_indexes_symid foreign key (symid) references lxr_symbols(symid),
-	constraint lxr_fk_indexes_relsym foreign key (relsym) references lxr_symbols(symid)
+	constraint fk_lxr_indexes_symid foreign key (symid) references lxr_symbols(symid),
+	constraint fk_lxr_indexes_fileid foreign key (fileid) references lxr_files(fileid),
+	/*constraint fk_lxr_indexes_langid foreign key (langid, type) references lxr_declarations(langid, declid),
+	*/
+	
+	constraint fk_lxr_indexes_relsym foreign key (relsym) references lxr_symbols(symid)
 );
+
 create index lxr_i_indexes on lxr_indexes(symid);
 
 commit;
@@ -51,8 +93,8 @@ commit;
 create table lxr_releases (	
 	fileid		number,
 	release		varchar2(250),
-	constraint lxr_pk_releases primary key (fileid,release),
-	constraint lxr_fk_releases_fileid foreign key (fileid) references lxr_files(fileid)
+	constraint pk_lxr_releases primary key (fileid,release),
+	constraint fk_lxr_releases_fileid foreign key (fileid) references lxr_files(fileid)
 );
 
 commit;
@@ -60,19 +102,35 @@ commit;
 create table lxr_status (
 	fileid		number,
 	status		number,
-	constraint lxr_pk_status primary key (fileid),
-	constraint lxr_fk_status_fileid foreign key (fileid) references lxr_files(fileid)
+	constraint pk_lxr_status primary key (fileid),
+	constraint fk_lxr_status_fileid foreign key (fileid) references lxr_files(fileid)
 );
 
 commit;
 
-create table lxr_usage (				
+create table lxr_usage (
 	fileid		number,
 	line		number,
 	symid		number,
-	constraint lxr_fk_usage_fileid foreign key (fileid) references lxr_files(fileid),
-	constraint lxr_fk_usage_symid foreign key (symid) references lxr_symbols(symid)
+	constraint fk_lxr_usage_fileid foreign key (fileid) references lxr_files(fileid),
+	constraint fk_lxr_usage_symid foreign key (symid) references lxr_symbols(symid)
 );
+
 create index lxr_i_usage on lxr_usage(symid);
+
+
+--grants
+
+
+grant select                         on lxr_filenum        to lxr;
+grant select                         on lxr_symnum         to lxr;
+grant select                         on lxr_declnum         to lxr;
+grant select, insert, update, delete on lxr_indexes        to lxr;
+grant select, insert, update, delete on lxr_usage          to lxr;
+grant select, insert, update, delete on lxr_symbols        to lxr;
+grant select, insert, update, delete on lxr_releases       to lxr;
+grant select, insert, update, delete on lxr_status         to lxr;
+grant select, insert, update, delete on lxr_files          to lxr;
+grant select, insert, update, delete on lxr_declarations   to lxr;
 
 commit;
