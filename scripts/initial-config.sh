dd2839d12ef4b6e2c7c7c0c001fd1b178c7d2c5f
André Littoz <ajlittoz@users.sourceforge.net>
2012-04-03 16:06:27 +0000

    scripts/: upgrade to 0.12 features
    initdb-config.sh: minor optimisation on sed call
    initial-config.sh: allow for different source templates
    set-lxr-version.sh: new script to set LXR version number (release and user); the idea is also to later get rid of LXRversion.pm by having the version number location knowledge inside this script

--- scripts/initial-config.sh
+++ scripts/initial-config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# $Id: initial-config.sh,v 1.7 2012/04/02 19:01:35 ajlittoz Exp $
+# $Id: initial-config.sh,v 1.8 2012/04/03 16:06:27 ajlittoz Exp $
 
 shopt -s extglob
 . ${0%%+([^/])}ANSI-escape.sh
@@ -51,71 +51,81 @@ if [[ ! -f "templates/$lxt" ]] ; then
 	exit 1
 fi
 
-echo "${VTyellow}***${VTnorm} Initial phase configurator for LXR (\$Revision: 1.7 $) ${VTyellow}***${VTnorm}"
+echo "${VTyellow}***${VTnorm} Initial phase configurator for LXR (\$Revision: 1.8 $) ${VTyellow}***${VTnorm}"
 echo
 
-while : ; do
-	read -p "Configure for single/multiple trees? [S/m] " cardinality
-	if [[ "$cardinality" ]] ; then
-		case "$cardinality" in
-			"s" | "S" )
-				cardinality="s"
-				break
-			;;
-			"m" | "M" )
-				cardinality="m"
-				break
-			;;
-			* )
-				echo "ERROR: invalid response, try again"
-				continue
-			;;
-		esac
-	else
-		cardinality="s"
-		break
-	fi
-done
+if [ "$everything" == 1 ] ; then
+	while : ; do
+		read -p "Configure for single/multiple trees? [S/m] " cardinality
+		if [ "x$cardinality" != "x" ] ; then
+			case "$cardinality" in
+				"s" | "S" )
+					cardinality="s"
+					break
+				;;
+				"m" | "M" )
+					cardinality="m"
+					break
+				;;
+				* )
+					echo "{VTred}ERROR:${VTnorm} invalid response, try again"
+					continue
+				;;
+			esac
+		else
+			cardinality="s"
+			break
+		fi
+	done
+fi
 
 lxr_root=`pwd`
 echo
-echo "Your LXR root directory is: ${VTbold}$lxr_root${VTnorm}"
+echo "Your LXR root directory is: ${VTbold}${lxr_root}${VTnorm}"
 echo
 # Escape the path separator (also regexp delimitor)
 lxr_root="${lxr_root//\//\\/}"
 
-confdir="lxrconf.d"
+if [ "$everything" == 1 ] ; then
 
-chmod -R a=r templates
-echo "templates directory now protected read-only"
+	chmod -R a=rx templates
+	echo "templates directory now protected read-only"
 
-target=".htaccess"
-cp templates/Apache/htaccess-generic $target
-echo "File ${VTbold}$target${VTnorm} written in your LXR root directory"
-echo "--- List its content with 'more $target'"
+	target=".htaccess"
+	cp templates/Apache/htaccess-generic ${target}
+	chmod u=rwx ${target}
+	echo "File ${VTbold}${target}${VTnorm} written in your LXR root directory"
+	echo "--- List its content with 'more $target'"
 
-target="apache2-require.pl"
-sed -e "s/%LXRroot%/$lxr_root/g" templates/Apache/$target > $confdir/$target
-echo "File ${VTbold}$target${VTnorm} written in $confdir directory"
+	target="apache2-require.pl"
+	sed -e "s/%LXRroot%/${lxr_root}/g" templates/Apache/$target > $confdir/$target
+	echo "File ${VTbold}$target${VTnorm} written in $confdir directory"
 
-target="apache-lxrserver.conf"
-sed -e "s/%LXRroot%/$lxr_root/g" templates/Apache/$target \
-	| sed -e "s/#=$cardinality=//" > $confdir/$target
-echo "File ${VTbold}$target${VTnorm} written in $confdir directory"
+	target="apache-lxrserver.conf"
+	sed -e "s/%LXRroot%/${lxr_root}/g" \
+		-e "s/#=$cardinality=//" \
+		templates/Apache/$target > $confdir/$target
+	echo "File ${VTbold}${target}${VTnorm} written in $confdir directory"
 
-target="lighttpd-lxrserver.conf"
-sed -e "s/%LXRroot%/$lxr_root/g" templates/lighttpd/$target \
-	> $confdir/lighttp-lxrserver.conf
-echo "File ${VTbold}$target${VTnorm} written in $confdir directory"
-if [[ "$cardinality" == "m" ]] ; then
-	echo "${VTyellow}You need to manually configure ${VTnorm}${VTbold}$target ${VTyellow}for multiple trees operation${VTnorm}"
+	target="lighttpd-lxrserver.conf"
+	sed -e "s/%LXRroot%/${lxr_root}/g" templates/lighttpd/$target \
+		> $confdir/lighttp-lxrserver.conf
+	echo "File ${VTbold}${target}${VTnorm} written in $confdir directory"
+	if [ "$cardinality" == "m" ] ; then
+		echo "${VTyellow}You need to manually configure ${VTnorm}${VTbold}$target ${VTyellow}for multiple trees operation${VTnorm}"
+	fi
+
+	chmod u=rwx,go=rx ${confdir}/*
+
 fi
 
 # lxr.conf pre-configuration
 
-lc="$confdir/lxr.conf"		# lxr.conf destination
-cp templates/lxr.conf $lc
-sed -e "s/%LXRroot%/$lxr_root/g" -i $lc
+lc="${confdir}/${lxt}"		# lxr.conf destination
+cp templates/${lxt} $lc
+chmod u=rwx ${lc}
+
+sed -e "s/%LXRroot%/${lxr_root}/g" -i $lc
 
 glimpse=`which glimpse 2>/dev/null`
 if [[ "$glimpse" ]] ; then	# glimpse exists
@@ -124,19 +134,20 @@ if [[ "$glimpse" ]] ; then	# glimpse exists
 	glimpseindex=`which glimpseindex`
 	if [[ "$glimpseindex" ]] ; then
 		glimpseindex="${glimpseindex//\//\\/}"
-		sed -e "s/%glimpseindex%/$glimpseindex/" -i $lc
+		sed -e "s/%glimpseindex%/${glimpseindex}/" -i $lc
 	else
 		echo "${VTred}***Error:${VTnorm} glimpseindex not installed with glimpse!"
 	fi
 else						# no glimpse
 	sed -e "/%glimpse%/s/^/#/" \
-	    -e "/%glimpseindex%/s/^/#/" -i $lc
+	    -e "/%glimpseindex%/s/^/#/" \
+		-i $lc
 fi
 
 swish=`which swish-e 2>/dev/null`
 if [[ "$swhish" ]] ; then		# swish-e exists
 	swish="${swish//\//\\/}"
-	sed -e "s/%swish%/$swish/" -i $lc
+	sed -e "s/%swish%/${swish}/" -i $lc
 else						# no swhish-e
 	sed -e '/%swish%/s/^/#/' -i $lc
 fi
@@ -153,12 +164,12 @@ fi
 ctagsbin=`which ctags`
 if [[ "$ctagsbin" ]] ; then		# ctags exists
 	ctagsbin="${ctagsbin//\//\\/}"
-	sed -e "s/%ctags%/$ctagsbin/" -i $lc
+	sed -e "s/%ctags%/${ctagsbin}/" -i $lc
 else						# no swhish-e
 	echo "${VTred}***Error:${VTnorm} ctags not installed!"
 fi
 
-echo "Prototype ${VTbold}lxr.conf${VTnorm} written in $confdir directory"
+echo "Prototype ${VTbold}${lxt}${VTnorm} written in $confdir directory"
 
 echo
 echo "${VTyellow}***${VTnorm} Configuration directory $confdir now contains: ${VTyellow}***${VTnorm}"
