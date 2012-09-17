dc6948b5f2b73d16d14f9cd7077f97176ff9c560
André Littoz <ajlittoz@users.sourceforge.net>
2012-09-17 12:15:43 +0000

    Lang.pm, Lang/*: restructure classes and comment them Markup.pm: recover language name from description
    Better isolate each language parsing from the others; allow to requeue a fragment tail for new processing if needed (this avoids recursive processing); use consistently 'identdef' for identifier parsing; add Python parser and improve the others

--- lib/LXR/Lang/Perl.pm
+++ lib/LXR/Lang/Perl.pm
@@ -1,11 +1,10 @@
 # -*- tab-width: 4 -*-
 ###############################################
 #
-# $Id: Perl.pm,v 1.9 2012/01/26 16:35:35 ajlittoz Exp $
+# $Id: Perl.pm,v 1.10 2012/09/17 12:15:43 ajlittoz Exp $
 #
-# Implements generic support for any language that ectags can parse.
-# This may not be ideal support, but it should at least work until
-# someone writes better support.
+# Enhances the support for the Perl language over that provided by
+# Generic.pm
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,19 +19,52 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+###############################################
+
+=head1 Perl language module
+
+This module is the Perl language highlighting engine.
+
+It shares most of its methods with I<Generic.pm>.
+It only overrides C<processinclude> for efficiency.
+
+=cut
 
 package LXR::Lang::Perl;
 
-$CVSID = '$Id: Perl.pm,v 1.9 2012/01/26 16:35:35 ajlittoz Exp $ ';
+$CVSID = '$Id: Perl.pm,v 1.10 2012/09/17 12:15:43 ajlittoz Exp $ ';
 
 use strict;
 use LXR::Common;
 use LXR::Lang;
 require LXR::Lang::Generic;
 
-@LXR::Lang::Perl::ISA = ('LXR::Lang::Generic');
+our @ISA = ('LXR::Lang::Generic');
 
-# Process a Perl include directive
+
+=head2 C<processinclude ($frag, $dir)>
+
+Method C<processinclude> is invoked to process a Perl I<include> directive.
+
+=over
+
+=item 1 C<$frag>
+
+a I<string> containing the directive
+
+=item 1 C<$dir>
+
+an optional I<string> containing a preferred directory for the include'd file
+
+=back
+
+This method does not reference the configuration file.
+However, the different reg-exps are still maintained there
+as check case for the generic parser.
+
+=cut
+
 sub processinclude {
 	my ($self, $frag, $dir) = @_;
 
@@ -43,22 +75,35 @@ sub processinclude {
 	my $path;		# OS include file
 	my $link;		# link to include file
 
-	$source =~ s/^					# reminder: no initial space in the grammar
-				([\w\#]\s*[\w]*)	# reserved keyword for include construct
-				(\s+)				# space
-				([\w:]+)\b
-				//sx ;
+	# Faster surrogate for 'directive'
+	if ($source !~ s/^		# reminder: no initial space in the grammar
+					([\w]+)	# reserved keyword for include construct
+					(\s+)	# space
+					([\w:]+)
+					//sx) {
+		# Guard against syntax error or variant
+		# Advance past keyword, so that parsing may continue without loop.
+		$source =~ s/^([\w]+)//;	# Erase keyword
+		$dirname = $1;
+		$$frag =	"<span class='reserved'>$dirname</span>";
+		&LXR::SimpleParse::requeuefrag($source);
+		return;
+	}
+
 	$dirname = $1;
 	$spacer  = $2;
 	$file    = $3;
 	$path    = $file;
 
-	$path =~ s@::@/@g;
-	$path =~ s@$@.pm@;
-	$link = &LXR::Common::incref($file, "include" ,$path ,$dir);
-	if ( defined($link)) {
+	# Faster surrogates for 'global' and 'last'
+	$path =~ s@::@/@g;		# Replace Perl delimiters
+	$path =~ s@$@.pm@;		# Add file extension
+
+	# Create the hyperlinks
+	$link = &LXR::Common::incref($file, "include", $path, $dir);
+	if (defined($link)) {
 		while ($file =~ m!::!) {
-			$link =~ s!^([^>]+>)([^:]*::)+!$1!g;
+			$link =~ s!^([^>]+>)([^:]*::)+!$1!;
 			$file =~ s!::[^:]*$!!;
 			$path =~ s!/[^/]+$!!;
 			$link = &LXR::Common::incdirref($file, "include" ,$path ,$dir)
@@ -68,13 +113,17 @@ sub processinclude {
 	} else {
 		$link = $file;
 	}
+
+	# As a goodie, rescan the tail of use/require for Perl code
+	&LXR::SimpleParse::requeuefrag($source);
+
+	# Assemble the highlighted bits
 	$$frag =	"<span class='reserved'>$dirname</span>"
 			.	$spacer
 			.	( defined($link)
 				? $link
 				: $file
-				)
-			. $source;
+				);
 }
 
 1;
